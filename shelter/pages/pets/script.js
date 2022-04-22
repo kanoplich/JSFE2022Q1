e1f3cf56629229f645beda49f7920cab959b5aee
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2022-04-23 00:17:48 +0300

    fead: disabled scroll

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,18 +1,20 @@
 const BURGER_BTN = document.querySelector('.header__burger');
 const MENU = document.querySelector('.header__burger__menu');
 const SHADOW = document.querySelector('.shadow_page');
+const BODY = document.body;
 
 const burger = () => {
   BURGER_BTN.classList.toggle('active');
   MENU.classList.toggle('active');
   SHADOW.classList.toggle('active');
+  BODY.classList.toggle('disabled-scroll');
 };
 
 const menu_burger = () => {
   MENU.classList.toggle('active');
   BURGER_BTN.classList.toggle('active');
   SHADOW.classList.toggle('active');
-
+  BODY.classList.toggle('disabled-scroll');
 };
 
 BURGER_BTN.addEventListener('click', burger);
