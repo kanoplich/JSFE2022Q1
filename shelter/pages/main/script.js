b4f0db8f680f1f81308640bf0903ae5e58f7ac82
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2022-04-25 20:11:45 +0300

    fix: change animation

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,3 +1,5 @@
+import pets from "./pets.json" assert { type: "json"};
+
 const BURGER_BTN = document.querySelector('.header__burger');
 const MENU = document.querySelector('.header__burger__menu');
 const SHADOW = document.querySelector('.shadow_page');
@@ -19,7 +21,8 @@ SHADOW.addEventListener('click', burger);
 const LEFT_BTN = document.querySelector('.left-button');
 const RIGHT_BTN = document.querySelector('.right-button');
 const CAROUSEL = document.querySelector('#carousel');
-
+const ITEM_LEFT = document.querySelector('#item-left');
+const ITEM_RIGHT = document.querySelector('#item-right');
 
 const moveLeft = () => {
   CAROUSEL.classList.add('transition-left');
@@ -33,9 +36,43 @@ const moveRight = () => {
   LEFT_BTN.removeEventListener('click', moveLeft);
 }
 
-const stopAnimation = () => {
-  CAROUSEL.classList.remove('transition-left');
-  CAROUSEL.classList.remove('transition-right');
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const createCard = () => {
+  let i = randomNumber(0, 7);
+
+  const card = document.createElement('div');
+  card.classList.add('friends__cards');
+  card.innerHTML = `<img class="cards" src="${pets[i].img}" alt="${pets[i].name}">
+  <h3 class="friends__name">${pets[i].name}</h3>
+  <div class="friends__cards__button">Learn more</div>
+  </div>`
+  return card;
+}
+
+const stopAnimation = (animationEvent) => {
+  let changeItem;
+  if(animationEvent.animationName === 'move-left') {
+    CAROUSEL.classList.remove('transition-left');
+    changeItem = ITEM_LEFT;
+    document.querySelector('#item-active').innerHTML = ITEM_LEFT.innerHTML;
+  } else {
+    CAROUSEL.classList.remove('transition-right');
+    changeItem = ITEM_RIGHT;
+    document.querySelector('#item-active').innerHTML = ITEM_RIGHT.innerHTML;
+  }
+
+  changeItem.innerHTML = "";
+  
+  for(let i = 0; i < 3; i++) {
+    const card = createCard();
+    changeItem.appendChild(card);
+  }
+
   LEFT_BTN.addEventListener('click', moveLeft);
   RIGHT_BTN.addEventListener('click', moveRight);
 }
@@ -43,3 +80,6 @@ const stopAnimation = () => {
 LEFT_BTN.addEventListener('click', moveLeft);
 RIGHT_BTN.addEventListener('click', moveRight);
 CAROUSEL.addEventListener('animationend', stopAnimation);
+
+
+
