aa4fa8587ed047ef27acb392a1781cbeaf14bccb
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2022-05-09 00:10:58 +0300

    feat: add event mousedown and mouseup

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -1,5 +1,6 @@
 const keyboard_code = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 const keyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', '◄', '▼', '►', 'Ctrl'];
+
 // let keyb = [];
 
 // document.onkeydown = function (event) {
@@ -18,10 +19,33 @@ function init() {
 init();
 
 document.onkeydown = function (event) {
-  console.log(`#keyboard .key ${event.code} active`);
   document.querySelector(`#keyboard .key[data=${event.code}]`).classList.add('active');
 }
 
 document.onkeyup = function (event) {
   document.querySelector(`#keyboard .key[data=${event.code}]`).classList.remove('active');
 }
+
+document.querySelectorAll('#keyboard .key').forEach(function(element) {
+  element.onmouseover = function(event) {
+    this.classList.add('hover');
+  }
+});
+
+document.querySelectorAll('#keyboard .key').forEach(function(element) {
+  element.onmouseout = function(event) {
+    this.classList.remove('hover');
+  }
+});
+
+document.querySelectorAll('#keyboard .key').forEach(function(element) {
+  element.onmousedown = function(event) {
+    this.classList.add('active');
+  }
+});
+
+document.querySelectorAll('#keyboard .key').forEach(function(element) {
+  element.onmouseup = function(event) {
+    this.classList.remove('active');
+  }
+});
